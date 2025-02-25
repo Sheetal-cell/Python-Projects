673766aa8b5073c7207b111f69160901e64f8abc
Sheetal <sheetalbajajgodda@gmail.com>
2025-02-25 19:35:56 +0530

    complete

--- KBC.py
+++ KBC.py
@@ -23,16 +23,16 @@ print("Welcome to Kaun Banega Crorepati-PYTHON Version😉")
 for i in range(0, len(questions)):
   
   question = questions[i]
-  print(f"\n\nQuestion for Rs. {levels[i]}")
+  print(f"\n\nQuestion for Rs. {levels[i]} 🤔")
   print(f"{question[0]}")
   print(f"1. {question[1]}          2. {question[2]} ")
   print(f"3. {question[3]}          4. {question[4]} ")
-  reply = int(input("Enter your answer (1-4) or  0 to quit:\n" ))
+  reply = int(input("Enter your answer (1-4) or  0 to quit:👇🏻\n" ))
   if (reply == 0):
     money = levels[i-1]
     break
   if(reply == question[-1]):
-    print(f"Correct answer, you have won Rs. {levels[i]}")
+    print(f"Correct answer, you have won Rs. {levels[i]}🤗")
     if(i<4):
       money=0
     elif(i >= 4 and i<9):
@@ -42,7 +42,7 @@ for i in range(0, len(questions)):
     elif(i == 14):
       money = 10000000
   else:
-    print("Wrong answer!")
+    print("Wrong answer!😞")
     break 
 
-print(f"Your take home money is {money}")
+print(f"Your take home money is {money} 🏆")
